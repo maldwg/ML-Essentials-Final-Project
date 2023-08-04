a3bf801ad87ea7eb2e51df27bcae0669e6762e17
Simon-0711 <Simon.pavicic99@gmail.com>
2023-08-04 14:16:24 +0200

    Add new agent baby_terminator and avatar image

---
+++ agent_code/baby_terminator/callbacks.py
@@ -0,0 +1,19 @@
+import os
+import pickle
+import random
+
+import numpy as np
+
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT']
+
+
+def setup(self):
+    np.random.seed()
+
+
+def act(agent, game_state: dict):
+    agent.logger.info('Pick action at random, but no bombs.')
+    print(game_state)
+    agent.logger.info(game_state)
+    return np.random.choice(ACTIONS)
