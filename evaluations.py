43908e0a92f65e6443ada4bb101cec6cdcdc7afa
Simon-0711 <Simon.pavicic99@gmail.com>
2023-08-06 16:20:48 +0200

    Add evaluation plot for q value and loss

--- evaluations.py
+++ evaluations.py
@@ -1,6 +1,7 @@
 import pickle
 from matplotlib.pylab import plt
 import numpy as np
+import torch
 
 
 AGENT_NAME = "baby_terminator"
@@ -9,34 +10,40 @@ model_path = "agent_code/" + AGENT_NAME + "/my-saved-model.pt"
 with open(model_path, "rb") as file:
     _, _, _, memory = pickle.load(file)
 
-print(memory)
-print(memory.q_value_after_episode)
-print(memory.loss_after_episode)
+# get q_value and calc mean
+q_value_after_episode = memory.q_value_after_episode
+mean_q_values_after_episode = []
+for batch_q_value in q_value_after_episode:
+    # Check that None is not passed
+    if isinstance(batch_q_value, torch.Tensor):
+        mean_q_value_after_episode = torch.mean(batch_q_value).item()
+        mean_q_values_after_episode.append(mean_q_value_after_episode)
+
+# get loss and calc mean
+loss_after_episode = memory.loss_after_episode
+mean_losses_after_episode = []
+for batch_loss in loss_after_episode:
+    # Check that None is not passed
+    if isinstance(batch_loss, torch.Tensor):
+        mean_loss_after_episode = torch.mean(batch_loss).item()
+        mean_losses_after_episode.append(mean_loss_after_episode)
 
 
-# # Load the training and validation loss dictionaries
-# train_loss = load(open('train_loss.pkl', 'rb'))
-# val_loss = load(open('val_loss.pkl', 'rb'))
+# Generate a sequence of integers to represent the epoch numbers
+epochs = range(1, len(mean_q_values_after_episode) + 1)
  
-# # Retrieve each dictionary's values
-# train_values = train_loss.values()
-# val_values = val_loss.values()
+# Plot and label the training and validation loss values
+plt.plot(epochs, mean_q_values_after_episode, label='Mean Q Value after each episode')
+plt.plot(epochs, mean_losses_after_episode, label='Mean Loss after each episode')
  
-# # Generate a sequence of integers to represent the epoch numbers
-# epochs = range(1, 21)
+# Add in a title and axes labels
+plt.title('Q Value and Loss after Episodes')
+plt.xlabel('Episodes')
+plt.ylabel('Loss and Q Value')
  
-# # Plot and label the training and validation loss values
-# plt.plot(epochs, train_values, label='Training Loss')
-# plt.plot(epochs, val_values, label='Validation Loss')
+# Set the tick locations
+plt.xticks(np.arange(0, len(mean_q_values_after_episode) + 1, 2))
  
-# # Add in a title and axes labels
-# plt.title('Training and Validation Loss')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
- 
-# # Set the tick locations
-# plt.xticks(arange(0, 21, 2))
- 
-# # Display the plot
-# plt.legend(loc='best')
-# plt.show()
+# Display the plot
+plt.legend(loc='best')
+plt.show()
