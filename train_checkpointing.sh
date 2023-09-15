f23e37859ecd5be3744b06626baf1ab3ca775d14
Simon-0711 <Simon.pavicic99@gmail.com>
2023-09-15 10:31:14 -0400

    update train and callbacks

--- train_checkpointing.sh
+++ train_checkpointing.sh
@@ -12,12 +12,11 @@ currentdatetime=$(date +"%Y%m%d%H%M")
 PARENT_DIR=agent_code/$AGENT/checkpoints/$currentdatetime
 mkdir -p $PARENT_DIR
 
-currentdatetime=$(date +"%Y%m%d%H%M")
 for idx in {1..4}
 do
     rounds=$(( CHECKPOINT * idx ))
     echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
+    mkdir -p  $PARENT_DIR/$rounds
     echo "Train the agent... ))"
     python main.py play --agents baby_terminator --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
     sleep 30
@@ -30,12 +29,11 @@ do
     echo "---------------------------------------------------"
 done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..6}
+for idx in {5..10}
 do
     rounds=$(( CHECKPOINT * idx ))
     echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
+    mkdir -p  $PARENT_DIR/$rounds
     echo "Train the agent... ))"
     python main.py play --agents baby_terminator peaceful_agent peaceful_agent coin_collector_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
     sleep 30
@@ -48,12 +46,11 @@ do
     echo "---------------------------------------------------"
 done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..8}
+for idx in {11..18}
 do
     rounds=$(( CHECKPOINT * idx ))
     echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
+    mkdir -p  $PARENT_DIR/$rounds
     echo "Train the agent... ))"
     python main.py play --agents baby_terminator --n-rounds=$CHECKPOINT --train 1 --scenario loot-crate --no-gui
     sleep 30
@@ -66,50 +63,13 @@ do
     echo "---------------------------------------------------"
 done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..20}
+for idx in {19..38}
 do
     rounds=$(( CHECKPOINT * idx ))
     echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
+    mkdir -p  $PARENT_DIR/$rounds
     echo "Train the agent... ))"
     python main.py play --agents baby_terminator peaceful_agent peaceful_agent coin_collector_agent --n-rounds=$CHECKPOINT --train 1 --scenario loot-crate --no-gui
-    sleep 30
-    echo "Training finished"
-    echo "copy the old model"
-    cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
-    echo "evaluate the agent"
-    python evaluations.py
-    cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
-    echo "---------------------------------------------------"
-done
-
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..20}
-do
-    rounds=$(( CHECKPOINT * idx ))
-    echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
-    echo "Train the agent... ))"
-    python main.py play --agents baby_terminator rule_based_agent rule_based_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario loot-crate --no-gui
-    sleep 30
-    echo "Training finished"
-    echo "copy the old model"
-    cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
-    echo "evaluate the agent"
-    python evaluations.py
-    cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
-    echo "---------------------------------------------------"
-done
-
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..30}
-do
-    rounds=$(( CHECKPOINT * idx ))
-    echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$currentdatetime/$rounds
-    echo "Train the agent... ))"
-    python main.py play --agents baby_terminator rule_based_agent rule_based_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario classic --no-gui
     sleep 30
     echo "Training finished"
     echo "copy the old model"
