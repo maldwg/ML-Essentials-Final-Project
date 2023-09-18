922d5e83e59252992ff14f10b5ae910864eedb4c
David Schwenke <schwenkedavid@t-online.de>
2023-09-18 16:54:43 +0200

    defined hyperparameter enums for grid search

---
+++ agent_code/baby_terminator/hyperparameters.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class Convolution(Enum):
+    OUTPUT_CHANNELS = "output_channels"
+    KERNEL_SIZE = "kernel_size"
+    STRIDE = "stride"
+    PADDING = "padding"
+    DROPOUT = "dropout"
+
+
+class Optimizer(Enum):
+    LEARNING_RATE = "lr"
+    WEIGHT_DECAY = "weight_decay"
+
+
+class Memory(Enum):
+    REPLAY_MEMORY_SIZE = "capacity"    
+
+
+class EpsilonGreedy(Enum):
+    EPS_START = "EPS_START"
+    EPS_END = "EPS_END"
+    EPS_DECAY = "EPS_DECAY"
+
+
+class Train(Enum):
+    BATCH_SIZE = "BATCH_SIZE"
+    GAMMA = "GAMMA"
