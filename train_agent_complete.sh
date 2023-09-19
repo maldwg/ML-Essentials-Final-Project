c9e44c5bf9ccfa039fde16416a0331c7b252ccd3
Simon-0711 <Simon.pavicic99@gmail.com>
2023-09-19 10:19:34 -0400

    Update another train script

--- train_agent_complete.sh
+++ train_agent_complete.sh
@@ -16,18 +16,18 @@ do
     echo "training on scenario $SCENARIO"
     echo "Training step 1 / 4"
     python main.py play --agents "$AGENT" peaceful_agent peaceful_agent random_agent --n-rounds="$N_ROUNDS_1" --train 1  --no-gui --scenario "$SCENARIO" 
-    python evaluations.py 
+    python evaluations_$AGENT.py 
     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
     echo "Training step 2 / 4"
     python main.py play --agents "$AGENT" peaceful_agent random_agent random_agent --n-rounds="$N_ROUNDS_2" --train 1  --no-gui --scenario "$SCENARIO" 
-    python evaluations.py
+    python evaluations_$AGENT.py
     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
     echo "Training step 3 / 4"
     python main.py play --agents "$AGENT"  peaceful_agent random_agent rule_based_agent --n-rounds="$N_ROUNDS_3" --train 1  --no-gui --scenario "$SCENARIO" 
-    python evaluations.py
+    python evaluations_$AGENT.py
     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
     echo "Training step 4 / 4"
     python main.py play --agents "$AGENT" rule_based_agent rule_based_agent rule_based_agent --n-rounds="$N_ROUNDS_4" --train 1  --no-gui --scenario "$SCENARIO" 
-    python evaluations.py
+    python evaluations_$AGENT.py
     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
 done
