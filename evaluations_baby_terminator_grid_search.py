7e9126d4a34de2b31268c332cbe1b07496a10c6b
Max Ludwig <maxludwig3@t-online.de>
2023-09-22 15:47:54 +0200

    finish evaluation python file

--- evaluations_baby_terminator_grid_search.py
+++ evaluations_baby_terminator_grid_search.py
@@ -3,36 +3,71 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import gzip
+import json
 from evaluations_utils import create_directory_if_not_exists, round_to_nearest_multiple
 import subprocess
 
+
 # parten directory containing all model files
 parent_directory = (
-    "agent_code/baby_terminator/checkpoints/350-coin-heaven-200-was-best/"
+    "agent_code/baby_terminator/top-five-models/"
 )
 pattern = "my-saved-model.pkl.gz"
+hyperparameters_pattern = "parameters.json"
 
 # dir for evaluation plots
 figure_evaluation_dir = (
-    "agent_code/baby_terminator/checkpoints/350-coin-heaven-200-was-best/evaluation/"
+    "agent_code/baby_terminator/top-five-models/"
 )
 create_directory_if_not_exists(figure_evaluation_dir)
 
-result = subprocess.run(
+model_result = subprocess.run(
     ["find", parent_directory, "-type", "f", "-name", pattern],
     capture_output=True,
     text=True,
 )
-if result.returncode == 0:
-    paths = result.stdout.strip().split("\n")
+if model_result.returncode == 0:
+    paths = model_result.stdout.strip().split("\n")
 else:
     print("Error occurred while searching for files.")
+
+
+hyperparameters_result = subprocess.run(
+    ["find", parent_directory, "-type", "f", "-name", hyperparameters_pattern],
+    capture_output=True,
+    text=True,
+)
+if hyperparameters_result.returncode == 0:
+    hyperparameters_paths = hyperparameters_result.stdout.strip().split("\n")
+else:
+    print("Error occurred while searching for files.")
+
+
+
+model_numbers = subprocess.run(
+    ["find", parent_directory, "-type", "d"],
+    capture_output=True,
+    text=True,
+)
+
+
+if model_numbers.returncode == 0:
+    model_numbers = model_numbers.stdout.strip().split("\n")[1:]
+else:
+    print("Error occurred while searching for files.")
+
+numbers = []
+for m_nr in model_numbers:
+    numbers.append("Model number: " + m_nr.split("/")[-1])
+
+model_numbers = numbers
 
 
 # List to store Q values, loss values, and rewards
 q_values = []
 losses = []
 rewards = []
+hyperparameters = []
 
 # Iterate over each path
 for model_path in paths:
@@ -62,9 +97,30 @@ for model_path in paths:
         # Get rewards
         rewards.append(memory.rewards_after_round)
 
+for p in hyperparameters_paths:
+    with open(p, "r") as f:
+        params = json.load(f)
+        hyperparameters.append(params)
+        f.close()
+
 # Plot Q values
-plt.figure()
-for mean_q_values_after_episode in q_values:
+params_as_strings = ""
+for params in hyperparameters:
+    formatted_string = ""
+    for dictionary in params:
+        for key, value in dictionary.items():
+            # Add LaTeX formatting for GAMMA and EPS variables
+            # if key == "GAMMA" :
+            #     key = f"${key}$"
+            # if key.startswith("EPS"):
+            #     key = f"${key.split("")[0}$ + key.split("")[1}"
+            formatted_string += f"{key.lower()}.: {value}, " 
+    params_as_strings += formatted_string + "\n"
+
+print(params_as_strings)
+
+plt.figure(figsize=(12, 6), dpi=100)
+for idx, mean_q_values_after_episode in enumerate(q_values):
     rounded_max_x = round_to_nearest_multiple(len(mean_q_values_after_episode))
     tick_interval = int(rounded_max_x / 5)
     if tick_interval == 0:
@@ -76,21 +132,23 @@ for mean_q_values_after_episode in q_values:
         stop_x_tick = rounded_max_x + 1
 
     epochs = range(1, len(mean_q_values_after_episode) + 1)
-    label = "Hyperparameters"
+    label = model_numbers[idx]
     plt.plot(epochs, mean_q_values_after_episode, label=label)
     plt.xticks(np.arange(0, stop_x_tick, tick_interval))
+
 
 plt.title("Q Values after Episodes for top 10 Grid Search models")
 plt.xlabel("Episodes")
 plt.ylabel("Q Value")
 
 plt.legend(loc="best")
+
 plt.savefig(f"{figure_evaluation_dir}grid_search_top_10_q_values.png")
 plt.clf()
 
 # Plot loss values
-plt.figure()
-for mean_losses_after_episode in losses:
+plt.figure(figsize=(12, 6), dpi=100)
+for idx, mean_losses_after_episode in enumerate(losses):
     rounded_max_x = round_to_nearest_multiple(len(mean_losses_after_episode))
     tick_interval = int(rounded_max_x / 5)
     if tick_interval == 0:
@@ -102,7 +160,7 @@ for mean_losses_after_episode in losses:
         stop_x_tick = rounded_max_x + 1
 
     epochs = range(1, len(mean_losses_after_episode) + 1)
-    label = "Hyperparameter"
+    label = model_numbers[idx]
     plt.plot(epochs, mean_losses_after_episode, label=label)
     plt.xticks(np.arange(0, stop_x_tick, tick_interval))
 
@@ -115,8 +173,8 @@ plt.savefig(f"{figure_evaluation_dir}grid_search_top_10_loss.png")
 plt.clf()
 
 # Plot rewards
-plt.figure()
-for rewards_after_round in rewards:
+plt.figure(figsize=(12, 6), dpi=100)
+for idx, rewards_after_round in enumerate(rewards):
     rounds = [x for x in range(1, len(rewards_after_round) + 1)]
 
     rounded_max_x = round_to_nearest_multiple(len(rewards_after_round))
@@ -129,7 +187,7 @@ for rewards_after_round in rewards:
     else:
         stop_x_tick = rounded_max_x + 1
 
-    plt.plot(rounds, rewards_after_round, label="Reward of round")
+    plt.plot(rounds, rewards_after_round, label=model_numbers[idx])
     plt.xticks(np.arange(0, stop_x_tick, tick_interval))
 
 plt.title("Overall reward after Round")
