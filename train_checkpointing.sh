7a2a54c6c2f3598dc8f259f1624cadbeb1def253
Max Ludwig <maxludwig3@t-online.de>
2023-09-25 12:56:13 +0200

    Trained best model

--- train_checkpointing.sh
+++ train_checkpointing.sh
@@ -13,7 +13,7 @@ PARENT_DIR=agent_code/$AGENT/checkpoints/$currentdatetime
 mkdir -p $PARENT_DIR
 
 currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {1..4}
+for idx in {3..4}
 do
     rounds=$(( CHECKPOINT * idx ))
     echo "new upper bound is ${rounds} rounds"
@@ -30,56 +30,56 @@ do
     echo "---------------------------------------------------"
 done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {5..6}
-do
-    rounds=$(( CHECKPOINT * idx ))
-    echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$rounds
-    echo "Train the agent... ))"
-    python main.py play --agents baby_terminator peaceful_agent peaceful_agent coin_collector_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
-    sleep 30
-    echo "Training finished"
-    echo "copy the old model"
-    cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
-    echo "evaluate the agent"
-    python evaluations_$AGENT.py
-    cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
-    echo "---------------------------------------------------"
-done
+# currentdatetime=$(date +"%Y%m%d%H%M")
+# for idx in {5..6}
+# do
+#     rounds=$(( CHECKPOINT * idx ))
+#     echo "new upper bound is ${rounds} rounds"
+#     mkdir -p  $PARENT_DIR/$rounds
+#     echo "Train the agent... ))"
+#     python main.py play --agents baby_terminator peaceful_agent peaceful_agent coin_collector_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
+#     sleep 30
+#     echo "Training finished"
+#     echo "copy the old model"
+#     cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
+#     echo "evaluate the agent"
+#     python evaluations_$AGENT.py
+#     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
+#     echo "---------------------------------------------------"
+# done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {7..8}
-do
-    rounds=$(( CHECKPOINT * idx ))
-    echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$rounds
-    echo "Train the agent... ))"
-    python main.py play --agents baby_terminator coin_collector_agent coin_collector_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
-    sleep 30
-    echo "Training finished"
-    echo "copy the old model"
-    cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
-    echo "evaluate the agent"
-    python evaluations_$AGENT.py
-    cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
-    echo "---------------------------------------------------"
-done
+# currentdatetime=$(date +"%Y%m%d%H%M")
+# for idx in {7..8}
+# do
+#     rounds=$(( CHECKPOINT * idx ))
+#     echo "new upper bound is ${rounds} rounds"
+#     mkdir -p  $PARENT_DIR/$rounds
+#     echo "Train the agent... ))"
+#     python main.py play --agents baby_terminator coin_collector_agent coin_collector_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
+#     sleep 30
+#     echo "Training finished"
+#     echo "copy the old model"
+#     cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
+#     echo "evaluate the agent"
+#     python evaluations_$AGENT.py
+#     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
+#     echo "---------------------------------------------------"
+# done
 
-currentdatetime=$(date +"%Y%m%d%H%M")
-for idx in {9..10}
-do
-    rounds=$(( CHECKPOINT * idx ))
-    echo "new upper bound is ${rounds} rounds"
-    mkdir -p  $PARENT_DIR/$rounds
-    echo "Train the agent... ))"
-    python main.py play --agents baby_terminator rule_based_agent rule_based_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
-    sleep 30
-    echo "Training finished"
-    echo "copy the old model"
-    cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
-    echo "evaluate the agent"
-    python evaluations_$AGENT.py
-    cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
-    echo "---------------------------------------------------"
-done
+# currentdatetime=$(date +"%Y%m%d%H%M")
+# for idx in {9..10}
+# do
+#     rounds=$(( CHECKPOINT * idx ))
+#     echo "new upper bound is ${rounds} rounds"
+#     mkdir -p  $PARENT_DIR/$rounds
+#     echo "Train the agent... ))"
+#     python main.py play --agents baby_terminator rule_based_agent rule_based_agent rule_based_agent --n-rounds=$CHECKPOINT --train 1 --scenario coin-heaven --no-gui
+#     sleep 30
+#     echo "Training finished"
+#     echo "copy the old model"
+#     cp $PARENT_DIR/../../my-saved-model.pkl.gz $PARENT_DIR/$currentdatetime/$rounds/
+#     echo "evaluate the agent"
+#     python evaluations_$AGENT.py
+#     cat agent_code/$AGENT/logs/$AGENT.log >> agent_code/$AGENT/logs/all.log
+#     echo "---------------------------------------------------"
+# done
